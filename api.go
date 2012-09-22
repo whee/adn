a97ef5b8849d6df579841dd1149b1c07f4570a5e
Brian Hetro <whee@smaertness.net>
2012-09-22 15:03:27 -0400

    Add endpoints for tokens, streams, posts, real-time updates, and filters

--- api.go
+++ api.go
@@ -22,7 +22,7 @@ type endpoint struct {
 var epTemplates = new(template.Template)
 
 var apiEndpoints = map[string]endpoint{
-	// User endpoints
+	// Users
 	"retrieve user":        {httpGet, "/stream/0/users/{{.UserId}}"},
 	"follow user":          {httpPost, "/stream/0/users/{{.UserId}}/follow"},
 	"unfollow user":        {httpDelete, "/stream/0/users/{{.UserId}}/follow"},
@@ -33,6 +33,44 @@ var apiEndpoints = map[string]endpoint{
 	"list muted users":     {httpGet, "/stream/0/users/me/muted"},
 	"list reposters":       {httpGet, "/stream/0/posts/{{.PostId}}/reposters"},
 	"list starrers":        {httpGet, "/stream/0/posts/{{.PostId}}/star"},
+
+	// Tokens
+	"check current token": {httpGet, "/stream/0/token"},
+
+	// Posts
+	"create post":                       {httpPost, "/stream/0/posts"},
+	"retrieve post":                     {httpGet, "/stream/0/posts/{{.PostId}}"},
+	"delete post":                       {httpDelete, "/stream/0/posts/{{.PostId}}"},
+	"retrieve post replies":             {httpGet, "/stream/0/posts/{{.PostId}}/replies"},
+	"retrieve user posts":               {httpGet, "/stream/0/users/{{.UserId}}/posts"},
+	"repost post":                       {httpPost, "/stream/0/posts/{{.PostId}}/repost"},
+	"unrepost post":                     {httpDelete, "/stream/0/posts/{{.PostId}}/repost"},
+	"star post":                         {httpPost, "/stream/0/posts/{{.PostId}}/star"},
+	"unstar post":                       {httpDelete, "/stream/0/posts/{{.PostId}}/star"},
+	"retrieve user starred posts":       {httpGet, "/stream/0/users/{{.UserId}}/stars"},
+	"retrieve posts mentioning user":    {httpGet, "/stream/0/users/{{.UserId}}/mentions"},
+	"retrieve user personalized stream": {httpGet, "/stream/0/posts/stream"},
+	"retrieve global stream":            {httpGet, "/stream/0/posts/stream/global"},
+	"retrieve tagged posts":             {httpGet, "/stream/0/posts/tag/{{.Hashtag}}"},
+
+	// Streams
+	"retrieve realtime user personalized stream":          {httpGet, "/stream/0/streams/user"},
+	"retrieve realtime multiple user personalized stream": {httpGet, "/stream/0/streams/app"},
+	"retrieve realtime public stream":                     {httpGet, "/stream/0/streams/public"},
+	"retrieve stream status":                              {httpGet, "/stream/0/streams/{{.StreamId}}"},
+	"control stream":                                      {httpPost, "/stream/0/streams/{{.StreamId}}"},
+
+	// Real-time updates
+	"list subscriptions":       {httpGet, "/stream/0/subscriptions"},
+	"create subscription":      {httpPost, "/stream/0/subscriptions"},
+	"delete subscription":      {httpDelete, "/stream/0/subscriptions/{{.SubscriptionId}}"},
+	"delete all subscriptions": {httpDelete, "/stream/0/subscriptions"},
+
+	// Filters
+	"retrieve current user filters": {httpGet, "/stream/0/filters"},
+	"create filter":                 {httpPost, "/stream/0/filters"},
+	"retrieve filter":               {httpGet, "/stream/0/filters/{{.FilterId}}"},
+	"delete filter":                 {httpGet, "/stream/0/filters/{{.FilterId}}"},
 }
 
 func init() {
